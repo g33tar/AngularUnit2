89df474014a69912da68c00a2dc9ea4a1265ee74
Tara Gee <geetar40@gmail.com>
2015-08-25 16:01:19 -0600

    fixed movie

--- Movie App/js/controllers.js
+++ Movie App/js/controllers.js
@@ -1,15 +1,17 @@
 
-app.controller('SearchController', function($scope, $http, $routeParams){
+app.controller('SearchController', function($scope, $http, $routeParams, $location){
   $scope.movieSearch = function(){
     $scope.movieTitle;
     $http.get('http://www.omdbapi.com/?s=' + $scope.movieTitle)
     .then(function(response){
+      $location.path('/')
       $scope.results = response.data.Search
     })
   }
   $http.get('http://www.omdbapi.com/?i=' + $routeParams.id)
     .then(function(response){
       $scope.movie = response.data
+
     })
    $scope.moviePoster = "http://img.omdbapi.com/?i=" + $routeParams.id + "&apikey=cb605cf9"
 })
